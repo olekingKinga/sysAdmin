0c72a8cceea154516fb95f802887876dad25a028
ole king Kinga <olekingkinga@gmail.com>
2024-06-28 10:18:49 +0300

    Added comments on add_user.py file line codes

--- add_user.py
+++ add_user.py
@@ -1,14 +1,14 @@
 # Script to add a new user to the Linux Account 
 # Correct
-import os
+import os #Import the os module to access interactive system() class 
 
-def new_user(): 
-    confirm = "N" # container / bag 
-    while confirm != "Y": # condition
-        username = input("Enter the name of the user to add: ")  # Bag username that store user input which is the name of the user
-        print("Use the username '" + username + "'? (Y/N)") 
-        confirm = input().upper()
+def new_user(): # define and initialize the python funtion
+    confirm = "N" # define and initialize a variable 
+    while confirm != "Y": # run a condition check against defined variable
+        username = input("Enter the name of the user to add: ")  # request user to insert his/her name
+        print("Use the username '" + username + "'? (Y/N)") # pull given name and direct user with kind of input to provide next 
+        confirm = input().upper() # convert user input into upper case if not
         
-    os.system("sudo adduser --force-badname " + username)
+    os.system("sudo adduser --force-badname " + username) # use module os and system() function in it to run the bash command
 
-new_user()
+new_user() # call the function
